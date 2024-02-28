ee82c3b0c65b18993f6b436a74a196d14d717f07
Laycon John <laycon.gomes@tld.local>
2024-02-28 15:41:18 -0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () => console.log("Running on ", process.env.PORT));
 }
 bootstrap();
