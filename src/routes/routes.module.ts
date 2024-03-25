c417ffaa55bd4eab0118910e4c305389a50642c1
Laycon John <laycon.gomes@tld.com.br>
2024-03-25 16:30:42 -0300

    chore: config typeorm repository on routes module

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -4,13 +4,28 @@ import { RoutesController } from './routes.controller';
 import { RouteInMemoryRepository } from 'src/@core/infra/db/in-memory/route-in-memory.repository';
 import { CreateRouteUseCase } from 'src/@core/application/create-route.use-case';
 import { ListAllRoutesUseCase } from 'src/@core/application/list-all-route.use-case';
+import { TypeOrmModule, getDataSourceToken } from '@nestjs/typeorm';
+import { RouteSchema } from 'src/@core/infra/db/typeorm/route.schema';
+import { RouteTypeOrmRepository } from 'src/@core/infra/db/typeorm/route-typeorm.repository';
+import { DataSource } from 'typeorm';
+import { Route } from '../@core/domain/route.entity';
 
 
 //TO_DO: implementar namespace para organizar
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([RouteSchema])
+  ],
   controllers: [RoutesController],
   providers: [
     RoutesService,
+    {
+      provide: RouteTypeOrmRepository,
+      useFactory: (dataSource: DataSource) => {
+        return new RouteTypeOrmRepository(dataSource.getRepository(Route));
+      },
+      inject: [getDataSourceToken()]
+    },
     {
       provide: RouteInMemoryRepository,
       useClass: RouteInMemoryRepository
