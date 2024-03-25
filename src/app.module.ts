db534b5e836b1ccd153592b2a052572b002cf2f1
Laycon John <laycon.gomes@tld.com.br>
2024-03-25 16:11:18 -0300

    chore: Typeorm config on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { RoutesModule } from './routes/routes.module';
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { RouteSchema } from './@core/infra/db/typeorm/route.schema';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), RoutesModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    RoutesModule,
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: join(__dirname, 'database.sqlite'),
+      synchronize: true,
+      logging: false,
+      entities: [RouteSchema]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
